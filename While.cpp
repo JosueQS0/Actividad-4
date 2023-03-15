b617ee6e33d123d1cda3f557e7d6f69b5fee957e
JosueQS0 <rquintaniilas@miumg.edu.gt>
2023-03-14 22:21:11 -0600

    Primer Commit

---
+++ While.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main(){
+	int i=0;	
+	while(i<5){
+		i++;
+		cout<<i<<endl;	
+	}
+	int dec=10;
+	cout<<"___________________"<<endl;
+	while(dec>0){
+		cout<<dec<<endl;
+		dec--;
+	}
+system("pause");
+}
